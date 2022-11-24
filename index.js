337af2078c587df1776f112f955e0948d1a14046
mitaly-dev <mitalyy.dev@gmail.com>
2022-11-23 21:00:51 -0800

    create api for dbuser

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const express = require('express')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000 
-
 
 app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ojtfupc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-app.get('/',(req,res)=>{
-    res.send({name:'mitaly'})
-})
+async function run(){
+    try{
+        const userCollection=client.db('FurnitureMarket').collection('users')
+
+        app.post('/users',async(req,res)=>{
+            const user = req.body 
+            const result = await userCollection.insertOne(user)
+            console.log(result)
+            res.send(result)
+         
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(error=>console.error(error.message))
+
 app.listen(port,()=>{
     console.log(`Furniture market is running on ${port}`)
 })
